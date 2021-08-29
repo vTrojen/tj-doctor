5b350c8c47527cdba49d84cb4df5ab2fe177440b
vTrojen <86936190+vTrojen@users.noreply.github.com>
2021-08-30 01:53:02 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.EnableBlips = false
+Config.EnablePeds = true
+
+Config.DoctorLimit = false
+Config.maxDoctor = 0
+Config.doctorPrice = 100
+Config.Doctor = {
+    {x = -468.670, y = 6287.023, z = 12.611, heading = -25.00, type = 'illegal'},
+--	{x = -1113.31, y = 4903.775, z = 218.59, heading = -35.00, type = 'illegal'},
+   -- {x = 816.9160, y = -492.400, z = 30.544, heading = 20.00, type = 'illegal'}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+
+game "gta5"
+
+author 'Trojen#9487'
+description 'tj-doctor'
+
+client_scripts {
+    'client/client.lua',
+    'config.lua'
+}
+
+server_scripts {
+    'server/server.lua',
+    'config.lua'
+}
